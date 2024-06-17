e76fc520fb9abae763c28c513ab677d878cf4ae1
SRE <Nerocross507@gmail.com>
2024-06-17 14:40:28 +0300

    first try send mail

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -7,4 +8,15 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.HiddenInput()
